edff43bc04438c80b0bc239dbfaf65ecd9ca923c
Edivaldo Silva <piqueno43@gmail.com>
2020-10-22 17:14:41 -0300

    Parcerias digitais eleições

--- dist/assets/js/shared/geral.js
+++ dist/assets/js/shared/geral.js
@@ -1,56 +1,57 @@
-
-
 $(document).ready(function () {
-	$("#pesquisar").keyup(function () {
-		var stringPesquisa = $(this).val();
-		$('.geral_perguntas div').parent().hide();
-		$('.geral_perguntas div:contains(' + stringPesquisa + ')').parent().show()
-	});
+  $('#pesquisar').keyup(function () {
+    var stringPesquisa = $(this).val()
+    $('.geral_perguntas div')
+      .parent()
+      .hide()
+    $('.geral_perguntas div:contains(' + stringPesquisa + ')')
+      .parent()
+      .show()
+  })
 
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 100) {
-			$('a[href="#top"]').fadeIn();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('a[href="#top"]').fadeIn()
+    } else {
+      $('a[href="#top"]').fadeOut()
+    }
+  })
 
-		} else {
-			$('a[href="#top"]').fadeOut();
+  $('a[href="#top"]').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: 0
+      },
+      800
+    )
+    return false
+  })
 
-		}
-	});
+  // carrega cabecalho nas páginas internas
+  // if (window.location.origin === 'http://www.justicaeleitoral.jus.br') {
+  //   $('#header').load(
+  //     'https://www.justicaeleitoral.jus.br/assets/inc/header.html #cabecalho_je'
+  //   )
+  // } else {
+  //   $('#header').load('../assets/inc/header.html #cabecalho_je')
+  // }
 
-	$('a[href="#top"]').click(function () {
-		$('html, body').animate({
-			scrollTop: 0
-		}, 800);
-		return false;
-	});
+  var $meuMenu = $('#menuFixo')
+  if ($meuMenu.length) {
+    var offset = $meuMenu.offset().top
 
-
-	// var offset = $('#menuFixo').offset().top;
-	// var $meuMenu = $('#menuFixo');
-	// $(document).on('scroll', function () {
-	// 	if (offset <= $(window).scrollTop()) {
-	// 		$meuMenu.addClass('fixar');
-	// 	} else {
-	// 		$meuMenu.removeClass('fixar');
-	// 	}
-	// });
-
-	var offset = $('#menuFixo').offset().top;
-	var $meuMenu = $('#menuFixo');
-	$(document).on('scroll', function () {
-		if (offset <= $(window).scrollTop()) {
-			$meuMenu.addClass('fixar');
-			$('.je-menu').addClass('menu-drop');
-			$('.je-menu ul').addClass('drop-content');
-			$('.btn-selecionar').addClass('btn-selecionar-visivel');
-		} else {
-			$meuMenu.removeClass('fixar');
-			$('.je-menu').removeClass('menu-drop');
-			$('.je-menu ul').removeClass('drop-content');
-			$('.btn-selecionar').removeClass('btn-selecionar-visivel');
-		}
-	});
-
-
-});
-
+    $(document).on('scroll', function () {
+      if (offset <= $(window).scrollTop()) {
+        $meuMenu.addClass('fixar')
+        $('.je-menu').addClass('menu-drop')
+        $('.je-menu ul').addClass('drop-content')
+        $('.btn-selecionar').addClass('btn-selecionar-visivel')
+      } else {
+        $meuMenu.removeClass('fixar')
+        $('.je-menu').removeClass('menu-drop')
+        $('.je-menu ul').removeClass('drop-content')
+        $('.btn-selecionar').removeClass('btn-selecionar-visivel')
+      }
+    })
+  }
+})
